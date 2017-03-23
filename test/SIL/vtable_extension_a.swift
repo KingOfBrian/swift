d9c07ada98fe4aee9666461c8f11fbe1f623d0fc
Brian King <brian.king@raizlabs.com>
2017-03-23 18:07:46 -0400

    Add tests to cover basic VTable support in extensions

---
+++ test/SIL/vtable_extension_a.swift
@@ -0,0 +1,23 @@
+// RUN: %target-swift-frontend -Xllvm -new-mangling-for-tests -emit-sil -O -primary-file %s %S/vtable_extension_b.swift -module-name main | %FileCheck %s
+
+
+class Base {
+  func inBD() {}
+}
+
+// CHECK: sil_vtable Base {
+// CHECK:   #Base.inBE!1: (Base) -> () -> () : _T04main4BaseC4inBEyyF	// Base.inBE() -> ()
+// CHECK:   #Base.inBD!1: (Base) -> () -> () : _T04main4BaseC4inBDyyF	// Base.inBD() -> ()
+// CHECK:   #Base.deinit!deallocator: _T04main4BaseCfD	// Base.__deallocating_deinit
+// CHECK:   #Base.init!initializer.1: (Base.Type) -> () -> Base : _T04main4BaseCACycfc	// Base.init() -> Base
+// CHECK: }
+
+
+extension Derived {
+  func inDE() {}
+
+  override func inBE() {}
+  override func inBD() {}
+}
+
+// CHECK-NOT: sil_vtable Derived {
---
+++ test/SIL/vtable_extension_b.swift
@@ -0,0 +1,23 @@
+// RUN: %target-swift-frontend -Xllvm -new-mangling-for-tests -emit-sil -O -primary-file %s %S/vtable_extension_a.swift -module-name main | %FileCheck %s
+
+extension Base {
+  func inBE() {}
+}
+
+class Derived: Base {
+  func inDD() {}
+}
+
+// CHECK: sil_vtable Derived {
+// CHECK:   #Base.inBD!1: (Base) -> () -> () : _T04main7DerivedC4inBDyyF	// Derived.inBD() -> ()
+// CHECK:   #Base.init!initializer.1: (Base.Type) -> () -> Base : _T04main7DerivedCACycfc	// Derived.init() -> Derived
+// CHECK:   #Base.inBE!1: (Base) -> () -> () : _T04main7DerivedC4inBEyyF	// Derived.inBE() -> ()
+// CHECK:   #Derived.inDE!1: (Derived) -> () -> () : _T04main7DerivedC4inDEyyF	// Derived.inDE() -> ()
+// CHECK:   #Derived.inDD!1: (Derived) -> () -> () : _T04main7DerivedC4inDDyyF	// Derived.inDD() -> ()
+// CHECK:   #Derived.deinit!deallocator: _T04main7DerivedCfD	// Derived.__deallocating_deinit
+// CHECK: }
+
+// CHECK-NOT: sil_vtable Base
+
+
+// FIXME: Add test for nested and local types
