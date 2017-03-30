356cfe422f6c8e2b6eddb3e81bc6289116a799c2
Brian King <brian.king@raizlabs.com>
2017-03-29 21:29:56 -0400

    Remove prints to cut down on SIL

--- test/SIL/vtable_extension_b.swift
+++ test/SIL/vtable_extension_b.swift
@@ -1,20 +1,20 @@
 // RUN: %target-swift-frontend -Xllvm -new-mangling-for-tests -emit-sil -O -primary-file %s %S/vtable_extension_a.swift %S/vtable_extension_c.swift -module-name main | %FileCheck %s
 
 extension Base {
-  static func inBDS() { print("Base.inBDS()") }
-  class func inBDC() { print("Base.inBDC()") }
-  func inBE() { print("Base().inBE()") }
-  final func finBE() { print("Base().finBE()") }
+  static func inBES() {  }
+  class func inBEC() {  }
+  func inBE() {  }
+  final func inBEF() {  }
 }
 
 class Derived: Base {
-  func inDD() { print("Derived().inDD()") }
+  func inDD() {  }
 }
 
 // CHECK:sil_vtable Derived {
 // CHECK-NEXT:  #Base.inBD!1: (Base) -> () -> () : _T04main7DerivedC4inBDyyF  // Derived.inBD() -> ()
 // CHECK-NEXT:  #Base.init!initializer.1: (Base.Type) -> () -> Base : _T04main7DerivedCACycfc // Derived.init() -> Derived
-// CHECK-NEXT:  #Base.inBDC!1: (Base.Type) -> () -> () : _T04main7DerivedC5inBDCyyFZ  // static Derived.inBDC() -> ()
+// CHECK-NEXT:  #Base.inBEC!1: (Base.Type) -> () -> () : _T04main7DerivedC5inBECyyFZ  // static Derived.inBEC() -> ()
 // CHECK-NEXT:  #Base.inBE!1: (Base) -> () -> () : _T04main7DerivedC4inBEyyF  // Derived.inBE() -> ()
 // CHECK-NEXT:  #Derived.inDE!1: (Derived) -> () -> () : _T04main7DerivedC4inDEyyF    // Derived.inDE() -> ()
 // CHECK-NEXT:  #Derived.inDD!1: (Derived) -> () -> () : _T04main7DerivedC4inDDyyF    // Derived.inDD() -> ()
--- test/SIL/vtable_extension_c.swift
+++ test/SIL/vtable_extension_c.swift
@@ -1,11 +1,11 @@
 // RUN: %target-swift-frontend -Xllvm -new-mangling-for-tests -emit-sil -O -primary-file %s %S/vtable_extension_a.swift %S/vtable_extension_b.swift -module-name main | %FileCheck %s
 
 extension Derived {
-  func inDE() { print("Derived().inDE()") }
+  func inDE() { }
 
-  override class func inBDC() { print("Derived.inBDC()") }
-  override func inBE() { print("Derived().inBE()") }
-  override func inBD() { print("Derived().inBD()") }
+  override class func inBEC() {  }
+  override func inBE() {  }
+  override func inBD() {  }
 }
 // CHECK-NOT: sil_vtable Derived {
 // CHECK-NOT: sil_vtable Base {
@@ -24,14 +24,19 @@ func invoke(baseDeclaration b: Base) {
 // CHECK-NEXT: apply [[REF]](%0) : $@convention(method) (@guaranteed Base) -> ()
 
 func invoke(baseClassExtension b: Base.Type) {
-  b.inBDC()
+  b.inBEC()
 }
-// CHECK: [[REF:%.*]] = class_method %0 : $@thick Base.Type, #Base.inBDC!1 : (Base.Type) -> () -> (), $@convention(method) (@thick Base.Type) -> ()
+// CHECK: [[REF:%.*]] = class_method %0 : $@thick Base.Type, #Base.inBEC!1 : (Base.Type) -> () -> (), $@convention(method) (@thick Base.Type) -> ()
+// CHECK-NEXT: apply [[REF]](%0) : $@convention(method) (@thick Base.Type) -> ()
+
+func invoke(baseStaticExtension b: Base.Type) {
+  b.inBES()
+}
+// CHECK: [[REF:%.*]] = function_ref @_T04main4BaseC5inBESyyFZ : $@convention(method) (@thick Base.Type) -> ()
 // CHECK-NEXT: apply [[REF]](%0) : $@convention(method) (@thick Base.Type) -> ()
 
 func invoke(finalBaseExtension b: Base) {
-  b.finBE()
+  b.inBEF()
 }
-// CHECK: [[REF:%.*]] = function_ref @_T04main4BaseC5finBEyyF : $@convention(method) (@guaranteed Base) -> ()
+// CHECK: [[REF:%.*]] = function_ref @_T04main4BaseC5inBEFyyF : $@convention(method) (@guaranteed Base) -> ()
 // CHECK-NEXT: apply [[REF]](%0) : $@convention(method) (@guaranteed Base) -> ()
-
